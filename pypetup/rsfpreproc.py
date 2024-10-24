a27c69809c2a73c13981357a02d95e00b1312458
Dhruman Goradia <Dhruman.Goradia2@bannerhealth.com>
2024-10-24 04:30:59 -0700

    changed the implementation for generate_rsfmat to make it significantly fast

--- pypetup/rsfpreproc.py
+++ pypetup/rsfpreproc.py
@@ -2,79 +2,80 @@ import os
 import argparse
 import nibabel as nib
 import numpy as np 
-import pandas as pd
-import itertools
 from fsl.wrappers import fslmaths
 from .misc import check_file_exists, check_fsl_installation, convert_3d_atlas_to_4d_binary_labels, filter_image
 
-def convert_mask_to_label(mask_nifti, block_size=64, start_label=20001):
+def convert_mask_to_label(mask_nifti_file, block_size=64, start_label=20001):
     """
-    Convert a Nifti mask image to a labeled Nifti image.
+    Convert a NIfTI mask image to a labeled NIfTI image.
 
-    Args:
-        mask_nifti (str): Path to the nifti mask image.
-        block_size (int, optional): Block size. Defaults to 64.
-        start_label (int, optional): Starting label. Defaults to 20001.
+    Parameters:
+    mask_nifti_file (str): Path to the NIfTI image representing the mask.
+    block_size (int): Size of the block to iterate over (default is 64).
+    start_label (int): Starting integer label for the active voxels (default is 20001).
 
     Returns:
-        labeled_nifti (str): Path to the labeled nifti image.
+    nibabel.Nifti1Image: A labeled NIfTI image.
     """
-    # Check FSL installation
-    version = check_fsl_installation()
-    print(f"FSL Version: {version}")
-
     # check if input files exist
-    if not check_file_exists(mask_nifti):
-        raise FileNotFoundError(f"Input mask file {mask_nifti} not found.")
+    if not check_file_exists(mask_nifti_file):
+        raise FileNotFoundError(f"Input mask file {mask_nifti_file} not found.")
     
     # get output file names and paths
-    output_dir = os.path.dirname(mask_nifti)
+    output_dir = os.path.dirname(mask_nifti_file)
     labeled_file = os.path.join(output_dir, "labeled_headmask_petfov.nii.gz")
 
-    # Load PET image
+    # Load the mask nifti file
     try:
-        mask_img = nib.load(mask_nifti)
-        # checking if the image shape is 3d
-        if len(mask_img.shape) != 3:
-            raise ValueError(f"Invalid image shape: {mask_img.shape}.\nOnly 3D mask images are supported.")
-        mask_data = mask_img.get_fdata
+        mask_nifti = nib.load(mask_nifti_file)
+        if len(mask_nifti.shape) != 3:
+            raise ValueError(f"Invalid image shape: {mask_nifti.shape}.\nOnly 3D mask images are supported.")
+        mask = mask_nifti.get_fdata()
     except nib.filebasedimages.ImageFileError:
-        raise ValueError(f"Error loading NIFTI file: {mask_nifti}")
+        raise ValueError(f"Error loading NIFTI file: {mask_nifti_file}")
     except Exception as e:
         raise RuntimeError(f"An unexpected error occurred: {e}")
-    
+
     # Create an empty label image of the same shape as mask
-    label_image = np.zeros_like(mask_img, dtype=np.int32)
+    label_image = np.zeros_like(mask, dtype=np.int32)
 
     # Define the label counter
     label_counter = start_label
 
+    # Get the shape of the mask
+    x_max, y_max, z_max = mask.shape
+
     # Iterate over the 3D image in blocks of the given size
-    for x, y, z in itertools.product(range(0, mask_img.shape[0], block_size),
-                                      range(0, mask_img.shape[1], block_size),
-                                      range(0, mask_img.shape[2], block_size)):
-        # Extract the current block
-        block = mask_data[x:x+block_size, y:y+block_size, z:z+block_size]
-        
-        # Check if the block contains any active voxels
-        if np.any(block):
-            # Assign the current label to all active voxels in the block
-            label_image[x:x+block_size, y:y+block_size, z:z+block_size][block > 0] = label_counter
-            # Increment the label counter
-            label_counter += 1
+    for x in range(0, x_max, block_size):
+        for y in range(0, y_max, block_size):
+            for z in range(0, z_max, block_size):
+                # Define the ending indices for the current block
+                x_end = min(x + block_size, x_max)
+                y_end = min(y + block_size, y_max)
+                z_end = min(z + block_size, z_max)
+
+                # Extract the current block
+                block = mask[x:x_end, y:y_end, z:z_end]
+
+                # Check if the block contains any active voxels
+                if np.any(block):
+                    # Assign the current label to all active voxels in the block
+                    label_image[x:x_end, y:y_end, z:z_end][block > 0] = label_counter
+                    # Increment the label counter
+                    label_counter += 1
 
     # Create a new NIfTI image from the labeled data, using the affine from the input mask
-    label_nifti = nib.Nifti1Image(label_image, affine=mask_nifti.affine)
+    labeled_nifti = nib.Nifti1Image(label_image, affine=mask_nifti.affine)
 
     # Save labeled image
     try:
-        nib.save(label_nifti, labeled_file)
+        nib.save(labeled_nifti, labeled_file)
     except IOError:
         raise IOError(f"Could not save PET sum file {labeled_file}")
-    
+
     return labeled_file
 
-def generate_rsfmat(image_path, label_path, output_csv_path):
+def generate_rsfmat(image_path, label_path, output_dir=None):
     """
     Extract mean values from each label for each 3D frame in a 4D image and save as an MxN matrix.
 
@@ -86,6 +87,11 @@ def generate_rsfmat(image_path, label_path, output_csv_path):
     Returns:
         None
     """
+
+    if output_dir is None:
+        output_dir = os.path.dirname(image_path)
+    output_file = os.path.join(output_dir, "rsfmat.txt")
+    
     try:
         # Load the 4D image and the 3D label file
         image_4d = nib.load(image_path)
@@ -103,35 +109,24 @@ def generate_rsfmat(image_path, label_path, output_csv_path):
         label_data = label_3d.get_fdata()
     except Exception as e:
         raise e
+    
+    # Get the unique labels in the atlas
+    unique_labels = np.unique(label_data)
+    unique_labels = unique_labels[unique_labels != 0] # Exclude the background label
 
-    # Get the unique labels excluding background (assuming label 0 is background)
-    labels = np.unique(label_data)
-    labels = labels[labels != 0]
+    # Extract mean time series for each ROI
+    mean_values = []
+    for label in unique_labels:
+        roi_mask = label_data == label
+        roi_values = image_data[roi_mask]
+        mean_values.append(np.mean(roi_values, axis=0))
+    # transposing mean_values to correctly store the rsfmat    
+    mean_values_t = np.array(mean_values).T
 
-    # Number of frames in the 4D image
-    if len(image_data.shape) != 4:
-        raise RuntimeError(f"Input Image {image_path} is not 4d.")
-    num_frames = image_data.shape[3]
-
-    # Initialize a matrix to store mean values (num_frames x num_labels)
-    mean_matrix = np.zeros((num_frames, len(labels)))
-
-    # Iterate through each frame and calculate mean values for each label
-    for frame in range(num_frames):
-        frame_data = image_data[:, :, :, frame]
-        for i, label in enumerate(labels):
-            # Extract the region corresponding to the current label
-            label_mask = label_data == label
-            # Calculate the mean value for the current label in the current frame
-            mean_value = frame_data[label_mask].mean()
-            mean_matrix[frame, i] = mean_value
-
-    # Save the output as a matrix file (CSV format)
-    try:
-        df = pd.DataFrame(mean_matrix, columns=[f'Label_{int(label)}' for label in labels])
-        df.to_csv(output_csv_path, index=False)
-    except Exception as e:
-        raise RuntimeError(f"Error saving CSV file: {e}")
+    # Save the rsfmat to a text file
+    np.savetxt(output_file, mean_values_t, delimiter='\t')
+    
+    return None
 
 def prepare_for_rsf(headmask_nifti, wmparc_nifti=None, sumall2t1_nifti=None):
     """
@@ -154,6 +149,10 @@ def prepare_for_rsf(headmask_nifti, wmparc_nifti=None, sumall2t1_nifti=None):
     Returns:
         None
     """
+    # Check FSL installation
+    version = check_fsl_installation()
+    print(f"FSL Version: {version}")
+
     output_dir = os.path.dirname(headmask_nifti)
     if wmparc_nifti is None:
         wmparc_nifti = os.path.join(output_dir, "wmparc.nii.gz")
@@ -177,8 +176,8 @@ def prepare_for_rsf(headmask_nifti, wmparc_nifti=None, sumall2t1_nifti=None):
     head_petfov = os.path.join(output_dir, "headmask_petfov.nii.gz")
     rsfmask = os.path.join(output_dir, "RSFMask.nii.gz")
     rsfmask4d = os.path.join(output_dir,"RSFMask_4D.nii.gz")
-    rsfmask4d_smth8 = os.path.join(output_dir,"RSFMask_4D_smth8mm.nii.gz")
-    rsfmat_csv = os.path.join(output_dir,"rsfmat.csv")
+    rsfmask4d_smth8 = os.path.join(output_dir,"RSFMask_4D_g8.nii.gz")
+    rsfmat_csv = os.path.join(output_dir,"rsfmat.txt")
 
     # Convert wmparc to bin
     try:
@@ -199,11 +198,11 @@ def prepare_for_rsf(headmask_nifti, wmparc_nifti=None, sumall2t1_nifti=None):
         raise RuntimeError(f"Encounted an unexpected error creating headmask in PET FOV: {e}")
     
     # Convert head mask in pet fov to labeled image. 
-    labeled_nifti = convert_mask_to_label(head_petfov)
+    labeled_file = convert_mask_to_label(head_petfov)
 
     # Create RSF Mask 
     try:
-        _ = fslmaths(labeled_nifti).add(wmparc_nifti).run(rsfmask, odt='int')
+        _ = fslmaths(labeled_file).add(wmparc_nifti).run(rsfmask, odt='int')
     except Exception as e:
         raise RuntimeError(f"Encounted an unexpected error creating RSF Mask: {e}")
     
@@ -214,7 +213,7 @@ def prepare_for_rsf(headmask_nifti, wmparc_nifti=None, sumall2t1_nifti=None):
     filter_image(rsfmask4d, rsfmask4d_smth8)
 
     # Create RSF matrix
-    generate_rsfmat(rsfmask4d_smth8, rsfmask, rsfmat_csv)
+    generate_rsfmat(rsfmask4d_smth8, rsfmask)
 
     return None
 
